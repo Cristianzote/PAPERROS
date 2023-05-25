2fd7563b1970b7e6b83172e17b0b280d907428d7
Emanuel Gómez Díaz <egomez413@misena.edu.co>
2023-05-25 18:06:00 -0500

    Homepage
    Homepage lista, pendiente de cambios que acordemos todos y de corregir unas cositas en el login

--- front/app/routes/homepage.routes.js
+++ front/app/routes/homepage.routes.js
@@ -4,37 +4,37 @@ const home = Router();
 
 home.get("/", (req, res)=>{
     res.render("inicio", {
-        "titulo":"PAPERROS", "activo":1
+        "titulo":"Paperros", "activo":1
     });
 });
 home.get("/Acerca", (req, res)=>{
     res.render("acerca", {
-        "titulo":"PAPERROS","activo":2
+        "titulo":"Paperros","activo":2
     });
 });
 home.get("/Servicios", (req, res)=>{
     res.render("servicios", {
-        "titulo":"PAPERROS","activo":3
+        "titulo":"Paperros","activo":3
     });
 });
 home.get("/Paseadores", (req, res)=>{
     res.render("paseadores", {
-        "titulo":"PAPERROS","activo":4
+        "titulo":"Paperros","activo":4
     });
 });
 home.get("/Contactanos", (req, res)=>{
     res.render("contactanos", {
-        "titulo":"PAPERROS","activo":5
+        "titulo":"Paperros","activo":5
         });
 });
 home.get("/Ingresa", (req, res)=>{
     res.render("ingresa", {
-        "titulo":"PAPERROS","activo":6
+        "titulo":"Paperros","activo":6
         });
 });
 home.get("/Registro", (req, res)=>{
     res.render("registro", {
-        "titulo":"PAPERROS","activo":7
+        "titulo":"Paperros","activo":7
         });
 });
 
